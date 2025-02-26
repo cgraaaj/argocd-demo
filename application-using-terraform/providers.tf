1aaafd982262e46b29f7c08c2c8fc36e1408cf8f
cgraaaj <cgraaaj@gmail.com>
2025-02-26 19:31:26 +0530

    added terraform

---
+++ application-using-terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    argocd = {
+      source = "argoproj-labs/argocd"
+      version = "7.3.1"
+    }
+  }
+}
+
+provider "argocd" {
+  server_addr = var.server_addr
+  username    = var.username
+  password    = var.password
+}
